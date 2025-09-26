ff4cbc5b85f3a23dccbd4cff711b80cfd2e83ed4
Trivedi-Krish <>
2025-09-26 14:59:18 -0600

    week3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,10 +1,10 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="hover:bg-sky-700 bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+    <li className="hover:bg-sky-700 bg-white rounded-lg px-6 py-8">
       <div className="flex justify-between items-center">
         <div>
-          <h3 className="font-bold text-lg text-grey-800">{name}</h3>
-          <p className="text-grey-600">Category: {category}</p>
+          <h3 className="font-bold text-lg text-black">{name}</h3>
+          <p className="text-black">Category: {category}</p>
         </div>
         <span className="text-xl font-medium dark:text-white bg-blue-500 text-blue-800 px-3 py-1 rounded-full font-semibold">
           Qty: {quantity}
